3d7f4fedb7cac982a70eed3123106b86670aee9b
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-11-09 21:54:01 +0530

    practised functions, loops, operators in js

---
+++ JavaScript_Basics/Loops.js
@@ -0,0 +1,43 @@
+///////////////////
+//// PROBLEM 1 ///
+/////////////////
+
+// Use a For Loop to print (console.log()) out the word "hello" 5 times.
+//
+// Do this with a While Loop and a For Loop
+
+// While Loop
+i = 1
+while(i<=5){
+    console.log("Hello")
+    i++
+}
+
+// For Loop
+for(i = 1;i<6;i++)
+{
+    console.log("hello")
+}
+
+
+
+/////////////////
+// PROBLEM 2 ///
+///////////////
+
+// Use Loops to console.log() (print out) all the odd numbers from 1 to 25
+// Do this using two methods, a while loop and a for loop
+
+// METHOD ONE
+// While Loop
+i = 1
+while(i<=25){
+    console.log(i)
+    i++
+}
+
+// METHOD TWO
+// For Loop
+for(i = 1; i< 26; i++){
+    console.log(i)
+}
