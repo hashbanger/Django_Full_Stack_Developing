8f8fa97872de84d25bdac8699ce2a82e2f7190a6
Prashant <prashant.brahmbhatt32@outlook.com>
2019-02-10 20:57:37 +0530

    added full code

--- JavaScript_Basics/00 - Beginner_Crash_Course.js
+++ JavaScript_Basics/00 - Beginner_Crash_Course.js
@@ -1,6 +1,10 @@
 // ## CRASH COURSE FOR BEGINNERS
 // Prashant Brahmbhatt (https://www.github.com/hashbanger)
 
+// It is suggested to use the console in some editor like vscode so you can instantly check the outputs of the snippets.
+// or go to codepen.io/
+
+
 var number  = 5 // in line comment
 
 /* multiline
@@ -803,16 +807,259 @@ console.log(magic());
 var myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
 
+// Higher order arrow functions
+const realNumberArray = [4, 5.6, -9.8, 3.14, 6, 7, 8.54];
+
+//the below snippet is just to show capabilities of arrow functions
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x* x);
+    return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+//Write higher order arrow functions
+const increment = (function() {
+    return function increment(number, value = 1){
+        return number + value;
+    };
+})();
+console.log(increment(5,2));
+console.log(increment(5));
+
+// The Rest Operartor
+const sum = (function(){
+    return function sum(...z){
+        const args = [x, y, z];
+        return args.reduce((a, b) => a + b, 0);
+    };
+})();
+console.log(sum(1,2,3,4));
+
+// Using the spread operator to evaluate arrays in place
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function(){
+   // arr2 = arr1; // does deep copy and changes the actual array
+   arr2 = [...arr1]; 
+   arr1[0] = 'potato';
+})();
+console.log(arr2);
+
+// USING DESTRUCTURING TO ASSIGN VARIABLES FROM OBJECTS -----------------------------
+var voxel = {x: 3.6, y: 7.4, z: 6.54};
+
+//the traditional way of copying
+var x = voxel.x;
+var y = voxel.y;
+var z = voxel.z;
+// using destructuring
+const {x: a, y: b, z: c} = voxel;
+console.log(x, y, z);
+
+//////////////////////////
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures){
+    'use strict';
+    const {tomorrow: tempOfTomorrow} = avgTemperatures;
+    return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES));
+
+// Destructuring assignment with nested objects
+const LOCAL_FORECAST = {
+    today: {min : 72, max: 83},
+    tomorrow: {min: 73.3, max: 84.6}
+};
+
+function getMaxOfTmrw(forecast){
+    'use strict';
+
+    const{tomorrow: {max: maxOfTomorrow}} = forecast;
+
+    return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST));
+
+// Destructuring assignment for arrays
+const[z, x] = [1,2,3,4,5,6];
+console.log(z,x);
+// Since we can't determine the array values to be copied unlike objects
+// we can use blanks to skip any object we don't want to copy
+const[z, , x, ,w] = [1,2,3,4,5,6];
+console.log(z, x, w);
+
+let a = 8, b = 6;
+(() => {
+    'use strict';
+    [a, b] = [b, a]
+})();
+console.log(a);
+console.log(b);
+
+// Using Destructuring with Rest operator
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list){
+
+    const [, , ... arr] = list;
+
+    return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr);
+console.log(source);
+
+// Using destructuring to pass an object as a function's parameters
+const stats = {
+    max: 56.6,
+    stddev: 4.432,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.76,
+    average: 35.56
+};
+
+const half = (function(){
+//  return function half(stats)
+    return function half( max, min) // more concise
+    {
+//      return (stats.max + stats.min) /2.0;
+        return (max + min)/ 2.0;
+    }
+})();
+// This method is generally used when there is a lot of iformation
+// in API calls then we require.
+
+// CREATE STRINGS USING TEMPLATE ---------------------------------------------------
+ 
+const person = {
+    name: 'Prashant Brahmbhatt',
+    age: 21
+};
+
+// using backticks
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+const result = {
+    success: ["max-length","no-and", "prefer-arrow-functions"],
+    failure: ["no-var","var-on-top", "linebreak"],
+    skipped: ["id-blacklist","no-dup-keys"]
+};
+
+function makeList(arr){
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++){
+        resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
+    return resultDisplayArray;
+}
+
+/**
+ * makeList(result.failure) should return:
+ * [`<li class = "text-warning">no-var</li>`,
+ *  `<li class = "text-warning">var-on-top</li>`,
+ *  `<li class = "text-warning">linebreak</li>`] 
+ **/
+
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray)
+
+// Write Concise Object Literal Declarations using Simple Fields
+
+// rather than using this below code we can do alternative
+// const createPerson = (name, age, gender) => {
+
+//     return {
+//         name: name,
+//         age: age,
+//         gender: gender
+//     };
+// };
+
+const createPerson = (name, age, gender) => ({name, age, gender});
+console.log(createPerson("Prashant Brahmbhatt", 21, "male"));
 
 
+// Write Concise Declarative Functions
+const bicycle = {
+    gear: 2,
+    //setGear: function(newGear)
+    setGear(newGear){
+        "use strict";
+        this.gear = newGear;
+    }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
 
+// Use class syntax to define a contructor function
+class SpaceShuttle{
+    constructor(targetPlanet){
+        this.targetPlanet = targetPlanet;
+    }
+}
+var zeus = new SpaceShuttle('Jupiter');
+console.log(zeus.targetPlanet)
 
+function makeClass(){
+    class Vegetable{
+        constructor(name){
+            this.name = name;
+        }
+    }
+    return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
 
+// USE GETTERS AND SETTERS --------------------------------------- ---------
+ class Book{
+     constructor(author){
+         this._author = author;
+     }
+// getter
+getwriter(){
+    return this._author;
+}
+//setter
+set writer(updatedAuthor){
+    this._author = updatedAuthor;
+}
+}
 
+function makeClass(){
+    class Thermostat{
+        constructor(temp){
+            this._temp = 5/9 * (temp -32);
+        }
+        get temperature(){
+            return this._temp;
+        }
+        set temperature(updatedTemp){
+            this._temp = updatedTemp;
+        }
+    }
+    return Thermostat;
+}
 
+const Thermostat = makeClass();
+const thermos = new Thermostat(76);
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature;
+console.log(temp);  
 
-
-
-
-//////////// de nada!
+//////////// de nada
